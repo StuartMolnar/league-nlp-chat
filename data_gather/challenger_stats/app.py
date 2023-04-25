bd6fd39727a5085bcf68f8c1da2cb5014e665556
StuartMolnar <stuartmolnar@hotmail.com>
2023-04-25 14:27:05 -0700

    file structure updates

--- data_gather/challenger_stats/app.py
+++
@@ -1,9 +0,0 @@
-from challenger_match_data import MatchData
-
-def main():
-    md = MatchData()
-    challenger_data = md.fetch_challenger_data()
-    print(challenger_data)
-
-if __name__ == '__main__':
-    main()
