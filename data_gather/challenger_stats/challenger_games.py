307cb550d568034ea0e2e3a8296731629e1845fe
StuartMolnar <stuartmolnar@hotmail.com>
2023-04-24 22:57:57 -0700

    gathered player data for all players

--- data_gather/challenger_stats/challenger_games.py
+++ data_gather/challenger_stats/challenger_games.py
@@ -3,6 +3,15 @@ import time
 import os
 import datetime
 from dotenv import load_dotenv
+import logging
+import logging.config
+import yaml
+
+with open('log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -16,34 +25,33 @@ class ChallengerGames:
         """
         Initialize the ChallengerGames object.
 
-        Call get_challenger_players() to fetch the challenger players from the Riot Games API.
-
-        Call the playerNameMap dict to get the player names used, mapped to their PUUID.
+        Call fetch_challenger_players() to fetch the challenger players from the Riot Games API.
         """
-        self.playerNameMap = {}
+        logger.info("ChallengerGames object initialized")
 
-    def get_challenger_players(self):
+    def fetch_challenger_players(self):
         """
         Fetch the challenger players from the Riot Games API.
 
         Returns:
             list: A list of challenger players, or an empty list if there's an error.
         """
+        logger.info("Fetching challenger players...")
         url = f'https://{REGION[0]}.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key={RIOT_KEY}'
         response = requests.get(url)
         
         if response.status_code != 200:
-            print(f"Error: status code {response.status_code}, response content: {response.content}")
+            logger.error(f"Status code {response.status_code}, response content: {response.content}")
             return []
 
         json_response = response.json()
         if 'entries' not in json_response:
-            print(f"Error: 'entries' key not found in response JSON: {json_response}")
+            logger.error(f"'entries' key not found in response JSON: {json_response}")
             return []
 
         return json_response['entries']
 
-    def get_puuid(self, summoner_id):
+    def fetch_puuid_by_summoner_id(self, summoner_id):
         """
         Fetch the PUUID (Player Universally Unique ID) of a player by their summoner ID.
 
@@ -53,23 +61,23 @@ class ChallengerGames:
         Returns:
             str: The player's PUUID, or None if there's an error.
         """
+        logger.info(f"Fetching PUUID for summoner ID: {summoner_id}")
         url = f'https://{REGION[0]}.api.riotgames.com/lol/summoner/v4/summoners/{summoner_id}?api_key={RIOT_KEY}'
         response = requests.get(url)
 
         if response.status_code != 200:
-            print(f"Error: status code {response.status_code}, response content: {response.content}")
+            logger.warning(f"Status code {response.status_code}, response content: {response.content}")
             return None
 
         data = response.json()
 
         if 'puuid' not in data or 'name' not in data:
-            print(f"Error: 'puuid' or 'name' key not found in response data: {data}")
+            logger.error(f"'puuid' or 'name' key not found in response data: {data}")
             return None
 
-        self.playerNameMap[data['puuid']] = data['name']
         return data['puuid']
 
-    def get_recent_matches(self, puuid, start_time):
+    def fetch_recent_matches_by_puuid(self, puuid, start_time):
         """
         Fetch the recent matches of a player by their PUUID.
 
@@ -80,6 +88,7 @@ class ChallengerGames:
         Returns:
             list: A list of recent match IDs, or an empty list if there's an error.
         """
+        logger.info(f"Fetching recent matches for PUUID: {puuid}")
         url = f'https://{REGION[1]}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?type=ranked&start=0&count=100&startTime={start_time}&api_key={RIOT_KEY}'
         response = requests.get(url)
 
@@ -87,14 +96,14 @@ class ChallengerGames:
             return response.json()
         elif response.status_code == 429:
             retry_after = int(response.headers.get('Retry-After', 1))
-            print(f"Rate limit exceeded. Waiting {retry_after} seconds before retrying.")
+            logger.info(f"Rate limit exceeded. Waiting {retry_after} seconds before retrying.")
             time.sleep(retry_after)
-            return self.get_recent_matches(puuid, start_time)
+            return self.fetch_recent_matches_by_puuid(puuid, start_time)
         else:
-            print(f"Error: status code {response.status_code}, response content: {response.content}")
+            logger.warning(f"Status code {response.status_code}, response content: {response.content}")
             return []
 
-    def get_challenger_games_past_day(self):
+    def fetch_challenger_games_from_past_day(self):
         """
         Fetch the challenger games that occurred in the past day.
 
@@ -103,13 +112,13 @@ class ChallengerGames:
         """
         current_time = datetime.datetime.utcnow()
         one_day_ago = int((current_time - datetime.timedelta(days=1)).timestamp())
-        challenger_players = self.get_challenger_players()
+        challenger_players = self.fetch_challenger_players()
         unique_game_ids = set()
 
         for player in challenger_players:
             summoner_id = player['summonerId']
-            puuid = self.get_puuid(summoner_id)
-            recent_matches = self.get_recent_matches(puuid, one_day_ago)
+            puuid = self.fetch_puuid_by_summoner_id(summoner_id)
+            recent_matches = self.fetch_recent_matches_by_puuid(puuid, one_day_ago)
 
             for match in recent_matches:
                 unique_game_ids.add(match)
@@ -123,9 +132,8 @@ def main():
     Fetches challenger games from the past day and prints the game IDs and player name map.
     """
     cg = ChallengerGames()
-    games_past_day = cg.get_challenger_games_past_day()
+    games_past_day = cg.fetch_challenger_games_from_past_day()
     print(games_past_day)
-    print(cg.playerNameMap)
 
 
 if __name__ == '__main__':
--- data_gather/challenger_stats/challenger_match_data.py
+++ data_gather/challenger_stats/challenger_match_data.py
@@ -5,6 +5,15 @@ import json
 import datetime
 from dotenv import load_dotenv
 from challenger_games import ChallengerGames
+import logging
+import logging.config
+import yaml
+
+with open('log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
 
 load_dotenv()
 
@@ -13,11 +22,15 @@ REGION = 'na1', 'americas'
 
 class MatchData:
     def __init__(self):
-        """ rewrite this """
+        """
+        Initialize the MatchData object.
+
+        Fetches the latest game data version from the Data Dragon API and initializes
+        the gameDataFiltered attribute as an empty list.
+        """
+        logger.info("MatchData object initialized")
         self.ddragonVersion = self.get_latest_ddragon_version()
         self.gameDataFiltered = []
-        # move challengergames from main function to here once module is complete
-        self.cg = ChallengerGames()
 
     def get_latest_ddragon_version(self):
         """
@@ -26,16 +39,17 @@ class MatchData:
         Returns:
             str: The latest version as a string, or None if there's an error.
         """
+        logger.info("Fetching the latest Data Dragon version")
         url = "https://ddragon.leagueoflegends.com/api/versions.json"
         response = requests.get(url)
         if response.status_code == 200:
             versions = response.json()
             return versions[0]  # Get the latest version
         else:
-            print(f"Error: status code {response.status_code}, response content: {response.content}")
+            logger.error(f"Status code {response.status_code}, response content: {response.content}")
             return None
         
-    def get_item_name(self, item_id):
+    def get_item_name_by_id(self, item_id):
         """
         Get the name of the in-game item by its ID.
 
@@ -45,6 +59,7 @@ class MatchData:
         Returns:
             str: The item's name as a string, or None if there's an error.
         """
+        logger.info(f"Fetching item name for item ID: {item_id}")
         language = "en_US"  # You can change this to another language if needed
         item_url = f"https://ddragon.leagueoflegends.com/cdn/{self.ddragonVersion}/data/{language}/item.json"
 
@@ -55,13 +70,13 @@ class MatchData:
                 item_name = items_data["data"][str(item_id)]["name"]
                 return item_name
             else:
-                print(f"Error: Item with ID {item_id} not found")
+                logger.info(f"Item with ID {item_id} not found")
                 return None
         else:
-            print(f"Error: status code {response.status_code}, response content: {response.content}")
+            logger.error(f"Status code {response.status_code}, response content: {response.content}")
             return     
 
-    def get_match_data(self, game_id):
+    def fetch_match_data_by_game_id(self, game_id):
         """
         Fetch match data for a specific game ID from the Riot Games API.
 
@@ -71,22 +86,22 @@ class MatchData:
         Returns:
             dict: The match data as a JSON object, or None if there's an error.
         """
+        logger.info(f"Fetching match data for game ID: {game_id}")
         url = f'https://{REGION[1]}.api.riotgames.com/lol/match/v5/matches/{game_id}?api_key={RIOT_KEY}'
         response = requests.get(url)
 
         if response.status_code == 200:
-            # print(f"Got match data for game {game_id}:", response.json())
             return response.json()
         elif response.status_code == 429:
             retry_after = int(response.headers.get('Retry-After', 1))
-            print(f"Rate limit exceeded. Waiting {retry_after} seconds before retrying.")
+            logger.info(f"Rate limit exceeded. Waiting {retry_after} seconds before retrying.")
             time.sleep(retry_after)
-            return self.get_match_data(game_id)
+            return self.fetch_match_data_by_game_id(game_id)
         else:
-            print(f"Error: status code {response.status_code}, response content: {response.content}")
+            logger.warning(f"Status code {response.status_code}, response content: {response.content}")
             return None        
             
-    def get_unique_team_positions(self, players):        
+    def extract_unique_team_positions(self, players):        
         """
         Extract unique team positions from a list of player dictionaries.
 
@@ -96,12 +111,13 @@ class MatchData:
         Returns:
             set: A set of unique team positions.
         """
+        logger.info("Extracting unique team positions")
         unique_positions = set()
         for player in players:
             unique_positions.add(player["teamPosition"])
         return unique_positions
     
-    def get_player_data(self, participant):
+    def extract_player_data_from_participant(self, participant):
         """
         Extracts and returns relevant player data from the participant dictionary.
 
@@ -112,19 +128,20 @@ class MatchData:
             list: A list containing the player's summoner name, champion name, team position,
                 kills, deaths, assists, and item names.
         """
+        logger.info("Extracting player data from participant")
         dataToGrab = ["summonerName", "championName", "teamPosition", "kills", "deaths", "assists"]
         data = []
         itemsToGrab = ["item0", "item1", "item2", "item3", "item4", "item5", "item6"]
         items = []
         for item in itemsToGrab:
-            items.append(self.get_item_name(participant[item]))
+            items.append(self.get_item_name_by_id(participant[item]))
         for data_item in dataToGrab:
             data.append(participant[data_item])
 
         player_data = data + items
         return player_data
 
-    def pair_players_by_position(self, players):
+    def group_players_by_position(self, players):
         """
         Pairs players by their team position and returns a dictionary of compositions.
 
@@ -135,48 +152,79 @@ class MatchData:
             dict: A dictionary with keys as team positions and values as lists of
                 player data for each team position.
         """
-        unique_positions = self.get_unique_team_positions(players)
+        logger.info("Grouping players by position")
+        unique_positions = self.extract_unique_team_positions(players)
         versus_compositions = {position: [] for position in unique_positions}
 
         for i in range(len(players)):
             team_position = players[i]["teamPosition"]
 
             if team_position in versus_compositions:
-                data = self.get_player_data(players[i])
+                data = self.extract_player_data_from_participant(players[i])
                 versus_compositions[team_position].append(data)
 
         return versus_compositions
         
-    def process_match_data(self, match_data):
-        """ In progress """
+    def process_single_match_data(self, match_data):
+        """
+        Process match data to extract and pair player data by team position.
+
+        Args:
+            match_data (dict): A dictionary containing the match data.
+
+        Returns:
+            dict: A dictionary with keys as team positions and values as lists of
+                player data for each team position.
+        """
+        logger.info("Processing single match data")
         players = match_data["info"]["participants"]
-        #self.get_player_data(players[0])
-        print(self.pair_players_by_position(players))
+        return self.group_players_by_position(players)
 
     def process_all_games(self, games):
-        """ In progress """
-        self.process_match_data(self.get_match_data(games[1]))
-        # for game in games:
-        #     match_data = self.get_match_data(game)
-        #     self.process_match_data(match_data)
+        """
+        Process a list of games, extracting and pairing player data by team position.
 
-    def main(self):
-        #!!! todo, finish process all games function
-        # prepare data for use in historical database service
-        # set up main file that will run the modules and send data to the database service
+        Args:
+            games (list): A list of game IDs.
 
+        Returns:
+            list: A list of dictionaries, each containing player data paired by team position.
+        """
+        logger.info("Processing all games")
+        processed_games = []
+        for game in games:
+            match_data = self.fetch_match_data_by_game_id(game)
+            processed_games.append(self.process_single_match_data(match_data))
 
+        return processed_games
 
+    def fetch_challenger_data(self):
+        """
+        Fetch and process match data for challenger games from the past day.
 
-        #module testing
+        Returns:
+            list: A list of dictionaries, each containing player data paired by team position.
+        """
+        logger.info("Fetching challenger match data")
         cg = ChallengerGames()
+        games = cg.fetch_challenger_games_from_past_day()
+        return self.process_all_games(games)
 
-        # Get challenger games from the past day
-        # games_past_day = cg.get_challenger_games_past_day()
-        games_past_day = ['NA1_4638633595', 'NA1_4638619939', 'NA1_4638740278', 'NA1_4638454797', 'NA1_4638500101', 'NA1_4638732418', 'NA1_4638622740', 'NA1_4638582501', 'NA1_4638655280', 'NA1_4638478680', 'NA1_4638810106', 'NA1_4638526935', 'NA1_4638568573', 'NA1_4638390819', 'NA1_4638799303', 'NA1_4638640572', 'NA1_4638625289', 'NA1_4638389920', 'NA1_4638547789', 'NA1_4638542108', 'NA1_4638453528', 'NA1_4638677124', 'NA1_4638485235', 'NA1_4638668657', 'NA1_4638769187', 'NA1_4638679608', 'NA1_4638662477', 'NA1_4638714098', 'NA1_4638621892', 'NA1_4638631866', 'NA1_4638689177', 'NA1_4638566386', 'NA1_4638629556', 'NA1_4638850853', 'NA1_4638501707', 'NA1_4638786771', 'NA1_4638398551', 'NA1_4638677565', 'NA1_4638544648', 'NA1_4638638258', 'NA1_4638680652', 'NA1_4638649249', 'NA1_4638611224', 'NA1_4638561991', 'NA1_4638732992', 'NA1_4638792285', 'NA1_4638802503', 'NA1_4638721939', 'NA1_4638558263', 'NA1_4638714749', 'NA1_4638650254', 'NA1_4638581458', 'NA1_4638380080', 'NA1_4638874515', 'NA1_4638627196']
-        # print("Challenger games in the past day:")
-        
-        self.process_all_games(games_past_day)
+def main():
+    #!!! todo, finish process all games function
+    # prepare data for use in historical database service
+    # set up main file that will run the modules and send data to the database service
+
+
+    #module testing
+    # cg = ChallengerGames()
+
+    # Get challenger games from the past day
+    # games_past_day = cg.get_challenger_games_past_day()
+    games_past_day = ['NA1_4638633595', 'NA1_4638619939', 'NA1_4638740278', 'NA1_4638454797', 'NA1_4638500101', 'NA1_4638732418', 'NA1_4638622740', 'NA1_4638582501', 'NA1_4638655280', 'NA1_4638478680', 'NA1_4638810106', 'NA1_4638526935', 'NA1_4638568573', 'NA1_4638390819', 'NA1_4638799303', 'NA1_4638640572', 'NA1_4638625289', 'NA1_4638389920', 'NA1_4638547789', 'NA1_4638542108', 'NA1_4638453528', 'NA1_4638677124', 'NA1_4638485235', 'NA1_4638668657', 'NA1_4638769187', 'NA1_4638679608', 'NA1_4638662477', 'NA1_4638714098', 'NA1_4638621892', 'NA1_4638631866', 'NA1_4638689177', 'NA1_4638566386', 'NA1_4638629556', 'NA1_4638850853', 'NA1_4638501707', 'NA1_4638786771', 'NA1_4638398551', 'NA1_4638677565', 'NA1_4638544648', 'NA1_4638638258', 'NA1_4638680652', 'NA1_4638649249', 'NA1_4638611224', 'NA1_4638561991', 'NA1_4638732992', 'NA1_4638792285', 'NA1_4638802503', 'NA1_4638721939', 'NA1_4638558263', 'NA1_4638714749', 'NA1_4638650254', 'NA1_4638581458', 'NA1_4638380080', 'NA1_4638874515', 'NA1_4638627196']
+    # print("Challenger games in the past day:")
+    
+    # process_all_games(games_past_day)
 
 
 
